3cd19d3cd9905b0c72c65d42cb9c180a248b73ac
kmodise <kmodise9898@gmail.com>
2021-08-13 16:53:28 +0200

    feedback review: remove duplicates

--- Task09.js
+++ Task09.js
@@ -1,13 +1,46 @@
+function removeDuplicate(str, strLength){
+        var index = 0;
+ 
+        for (var i = 0; i < strLength; i++){
+            var j;
+            for (j = 0; j < i; j++){
+                if (str[i] == str[j]){
+                    break;
+                }
+            }
+            if (j == i){
+                str[index++] = str[i];
+            }
+        }
+        return str.join("").slice(str, index);
+    }
+
 function vowelCheck(string){
     var collectVowel = "";
     var i = 0;
-    
+
     while(string[i]){
         if (string[i] === 'a' || string[i] === 'e' || string[i] === 'i' || string[i] === 'o' || string[i] === 'u' ||
                 string[i] === 'A' || string[i] === 'E' || string[i] === 'I' || string[i] === 'O' || string[i] === 'U'){
-            collectVowel += string[i] + ',';
+            collectVowel += string[i];
         }
         i++;
     }
-    console.log(collectVowel);
+
+    var a = collectVowel.toString();
+    var removeUpperCase = a.toLocaleLowerCase();
+    var splitString = removeUpperCase.split("");
+    var finalResult = removeDuplicate(splitString, splitString.length);
+    
+
+    if (finalResult){
+        i = 0;
+        while(finalResult[i]){
+            process.stdout.write(finalResult[i] + ',');//used it to avoid a new line which console.log() adds
+            i++;
+        }
+    }
+    else {
+        console.log(finalResult);
+    }
 }
