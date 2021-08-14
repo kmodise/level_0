88da2e6aa5a6eb843b0570ed0694331d485ca09b
kmodise <kmodise9898@gmail.com>
2021-08-14 12:27:00 +0200

    removed duplicate characters

--- Task10.js
+++ Task10.js
@@ -1,19 +1,36 @@
-function sameCharacter(str_one, str_two){
+function removeDuplicateChar(str) {
+    return str.split('').filter(function(item, pos, self) {
+        return self.indexOf(item) == pos;
+      }).join('');
+  }
+
+
+function sameCharacter(s1, s2){
     var i = 0;
     var x = 0;
     var commonChar = "";
+    var s1 = s1.toLocaleLowerCase();
+    var s2 = s2.toLocaleLowerCase();
+    
 
-    while(str_one[i]){
+    while(s1[i]){
 
-        while(str_two[x]){
-            if (str_one[i] === str_two[x]){
-                commonChar += str_two[x] + ',';
+        while(s2[x]){
+            if (s1[i] === s2[x]){
+                commonChar += s2[x];
             }
             x++;
         }
         i++;
         x = 0;
-
     }
-    console.log(commonChar);
+
+    var setToLowerCase = commonChar.toLocaleLowerCase();
+    var finalResult = removeDuplicateChar(setToLowerCase);
+    i = 0;
+
+    while(finalResult[i]){
+        process.stdout.write(finalResult[i] + ',');//used it to avoid a new line which console.log() adds
+        i++;
+    }
 }
